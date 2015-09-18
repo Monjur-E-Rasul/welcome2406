519d6fd827fbf9428f5e7b0dc2b1c8e79c4dec7e
Monjur E Rasul Shaon <mosalma66@gmail.com>
2015-09-18 20:55:41 +0600

    message

--- TextBook.java
+++ TextBook.java
@@ -12,12 +12,10 @@ public class TextBook {
     Book b1("Teach Yourself C++", schildt);
     b1.setPrice(150.5);
     b1.setStock(50);
-    //cout << "Author: " << b1.getAuthorName() <<" Name: " << b1.getName() << " Price: " << b1.getPrice() << " Stock: " << b1.getStock() << endl;
     System.out.println("Author"+b1.getAuthorname()+" Name: "+b1.getName()+" Price: "+b1.getPrice()+" Stock: "+b1.getStock());
     b1.borrowBook(20);
     b1.returnBook(10);
-    //cout << "Author: " << b1.getAuthorName() <<" Name: " << b1.getName() << " Price: " << b1.getPrice() << " Stock: " << b1.getStock() << endl;
-
+    
     System.out.println("Author: "+b1.getAuthorname()+" Name: "+b1.getName()+" Price: "+b1.getPrice()+" Stock: "+b1.getStock());
 
     Book b2("The C++ Programming Language", bjarne, 200, 20);
